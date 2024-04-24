e0acdaacd1ab4c9d04ef0301a30fe3b7674a2f49
leeJeonghyunz <dlwjdus3217@naver.com>
2024-04-24 18:44:23 +0900

    mod4

--- src/theme.ts
+++ src/theme.ts
@@ -3,6 +3,6 @@ import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#636e72",
   textColor: "#dfe6e9",
-  accentColor: "#ffeaa7",
+  accentColor: "gold",
   infoBox: "#2d3436",
 };
